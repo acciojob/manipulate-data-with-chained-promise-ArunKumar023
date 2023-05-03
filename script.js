85ed6299c2410a90da658685514c1febe6f3f794
Arun kumar <106857719+ArunKumar023@users.noreply.github.com>
2023-05-03 07:18:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,30 @@
-function manipulateArray(arr) {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve(arr);
-    }, 3000);
-  })
-  .then(arr => {
-    return arr.filter(num => num % 2 === 0);
-  })
-  .then(evenArr => {
-    return evenArr.map(num => num * 2);
-  });  
-}
-
 const input = [1, 2, 3, 4];
 
-manipulateArray(input)
-  .then(result => {
-    const outputDiv = document.getElementById("output");
-    outputDiv.innerHTML = result.join(", ");
+const promise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve(input);
+  }, 3000);
+});
+
+const filterPromise = promise.then((arr) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const filteredArr = arr.filter((num) => num % 2 === 0);
+      resolve(filteredArr);
+    }, 1000);
   });
+});
+
+const mapPromise = filterPromise.then((arr) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const mappedArr = arr.map((num) => num * 2);
+      resolve(mappedArr);
+    }, 2000);
+  });
+});
+
+mapPromise.then((arr) => {
+  const outputDiv = document.getElementById("output");
+  outputDiv.innerText = arr.join(", ");
+});
